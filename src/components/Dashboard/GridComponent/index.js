847a7b96ba1293e259cd510c1eef0074e7299505
Rajesh Kannan <krishfredrick11@gmail.com>
2022-11-20 20:12:28 +0530

    screwedUp grid

--- src/components/Dashboard/GridComponent/index.js
+++ src/components/Dashboard/GridComponent/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import TrendingDownRoundedIcon from '@mui/icons-material/TrendingDownRound'
-import RoundedIcon from '@mui/icons-material/TrendingDownRound'
-import './styles.scss'
-import { motion } from 'framer-motion'
+import TrendingDownRoundedIcon from "@mui/icons-material/TrendingDownRounded";
+import TrendingUpRoundedIcon from "@mui/icons-material/TrendingUpRounded";
+import "./styles.scss";
+import { motion } from "framer-motion";
 
-import React from 'react'
 
 const Grid = ({ coin, delay}) => {
   return (
     <motion.div
       initial={{ y: 60, opacity: 0}}
-      whileInView={{duration:0.3, delay: delay}}
+      whileInView={{y: 0, opacity: 1}}
       transition={{duration:0.3, delay: delay}}
       className={`grid__box 
       ${coin.price_change_percentage_24h < 0 && 'grid__box-red'}`}
@@ -18,8 +17,8 @@ const Grid = ({ coin, delay}) => {
       <div className="grid__info-flex">
         <img src={coin.image} alt="" className="grid__coin-logo" />
           <div className="grid__name-flex">
-            <div className="grid__coin-symbol">{coin.symbol}</div>
-            <div className="grid__coin-name">{coin.name} </div>
+            <p className="grid__coin-symbol">{coin.symbol}</p>
+            <p className="grid__coin-name">{coin.name} </p>
           </div>
       </div>
       <div>
@@ -28,9 +27,9 @@ const Grid = ({ coin, delay}) => {
               <div className="grid__coin-chip">
                 {coin.price_change_percentage_24h.toFixed(2) + '%'}
               </div>
-              <TrendingUpRoundedIcon className='grid__icon' />
+              <TrendingUpRoundedIcon className="grid__icon" />
             </div>
-        ):(
+        ) : (
           <div className="grid__chip-flex">
             <div className="grid__coin-chip chip-red">
               {coin.price_change_percentage_24h.toFixed(2) +'%'}
@@ -51,14 +50,16 @@ const Grid = ({ coin, delay}) => {
       </p>
       <div>
         <p className="grid__volume-text">
-          <strong>Total Volume:</strong> ${coin.total_volume.toLocaleString()}
+          <strong>Total Volume:</strong> 
+          ${coin.total_volume.toLocaleString()}
         </p>
         <p className="grid__volume-text">
-          <strong>Total Market cap:</strong> ${coin.total_volume.toLocaleString()}
+          <strong>Total Market cap:</strong> 
+          ${coin.total_volume.toLocaleString()}
         </p>
       </div>
     </motion.div>
   )
 }
 
-export default Grid
+export default Grid;
---
+++ src/components/Dashboard/Tabs/index.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { createTheme, ThemeProvider } from "@mui/material";
+import Grid from "../GridComponent/";
+import "./styles.scss";
+import List from "../ListComponent/";
+export default function Tabs({ data }) {
+  const [tabValue, setTabValue] = useState("grid");
+
+  const handleChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
+  const style = {
+    color: "var(--white)",
+    width: "50vw",
+    fontSize: "1.2rem",
+    fontWeight: 600,
+    fontFamily: "Inter",
+    textTransform: "capitalize",
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3a80e9",
+      },
+    },
+  });
+
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <TabContext value={tabValue}>
+          <div>
+            <TabList variant="fullWidth" onChange={handleChange}>
+              <Tab label="Grid" value={"grid"} sx={style} />
+              <Tab label="List" value={"list"} sx={style} />
+            </TabList>
+          </div>
+          <TabPanel value={"grid"}>
+            <div className="tab__grid-flex">
+              {data.map((item, i) => (
+                <Grid coin={item} key={i} delay={(i % 5) * 0.1} />
+              ))}
+            </div>
+          </TabPanel>
+          <TabPanel value={"list"}>
+            <table className="tab__list-flex">
+              {data.map((item, i) => (
+                <List coin={item} delay={(i % 7) * 0.1} />
+              ))}
+            </table>
+          </TabPanel>
+        </TabContext>
+      </ThemeProvider>
+    </div>
+  );
+}
